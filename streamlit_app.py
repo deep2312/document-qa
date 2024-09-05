b525947a2e04d1d1e1f25e33c7c4a4160faebc45
deep2312 <67276364+deep2312@users.noreply.github.com>
2024-09-05 11:44:50 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,10 @@
 import streamlit as st
+from lab1 import lab1
+from lab2 import lab2
 
 pg = st.navigation([
-    st.Page(lab1, title = "Lab 1"),
-    st.Page(lab2, title = "Lab 2")
+    st.Page(lab2, title = "Lab 2"),
+    st.Page(lab1, title = "Lab 1")
 ], position = "top")
 
 
